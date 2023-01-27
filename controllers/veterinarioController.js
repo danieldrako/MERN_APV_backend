d32ebc5be3e3d1bbffa10adffd257a61b4fa54ce
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-01-26 21:58:47 -0600

    after firsts steps with confirm account

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -5,7 +5,6 @@ const registrar = async (req,res) => {
 
     // Prevenir usuarios duplicados
     const existeUsuario = await Veterinario.findOne({email})
-
     if(existeUsuario){
         const error = new Error('Ya existe un usuario registrado con este correo');
         return res.status(400).json({msg: error.message})
@@ -15,18 +14,21 @@ const registrar = async (req,res) => {
         //*Guardar un nuevo veterinario
         const veterinario = new Veterinario(req.body);
         const veterinarioGuardado = await  veterinario.save();
-        
+
         res.json(veterinarioGuardado)
     } catch (error) {
         console.log(error)
     }
-
-
-
  };
 
 const perfil =  (req,res) => { 
     res.json({msg:"mostrando perfil"})
  };
 
- export {registrar, perfil }; 
+ const confirmar = (req,res) =>{
+    console.log(req.params.token)//?para leer datos de la url se usa req.params
+    
+    res.json({msg:'Confirmando cuenta...'})
+ }
+
+ export {registrar, perfil, confirmar }; 
