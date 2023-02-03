17669583e883321039be6f981ce131eb5ccd28ea
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-02-02 22:27:47 -0600

    afterChecking if the user requesting the Email Exists

--- controllers/veterinarioController.js
+++ controllers/veterinarioController.js
@@ -1,5 +1,6 @@
 import Veterinario from "../models/Veterinario.js";
 import generarJWT from "../helpers/generarJWT.js";
+import generarId from "../helpers/generarId.js";
 
 const registrar = async (req,res) => { 
     const { email} = req.body
@@ -73,7 +74,23 @@ const perfil =  (req,res) => {
     }    
  };
 
- const olvidePassword = (req,res) => {   }
+ const olvidePassword = async (req,res) => {  
+    const { email } = req.body
+
+    const existeVeterinario =  await Veterinario.findOne( { email } )
+    if(!existeVeterinario) {
+        const error = new Error('El usuario no existe');
+        return res.status(400).json( { msg: error.message });
+    }
+
+    try {
+        existeVeterinario.token = generarId()
+        await existeVeterinario.save()
+        res.json( { msg: "Revisa tu correo para restablecer tu contraseña" })
+    } catch (error) {
+        console.log(error)
+    }
+   }
 
  const comprobarToken = (req,res) => {   }
 
