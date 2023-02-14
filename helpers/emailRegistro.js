05baa65611cdf47dc5f3e7a6b7ad6ee57a097af5
Luis Daniel <ls.ortizdaniel@gmail.com>
2023-02-14 13:44:08 -0600

    after to send an email, from fronted module

--- helpers/emailRegistro.js
+++ helpers/emailRegistro.js
@@ -1,6 +1,6 @@
 import nodemailer from "nodemailer"
 
-const emailRegistro = () => { 
+const emailRegistro = async (datos) => { 
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
         port: process.env.EMAIL_PORT,
@@ -9,6 +9,26 @@ const emailRegistro = () => {
           pass: process.env.EMAIL_PASS,
         }
       });
+
+      const { email, nombre, token } = datos;
+
+      //! Enviar email
+
+      const info = await transporter.sendMail({
+        from: "APV- Administrador de Pacientes de Veterinaria",
+        to: email,
+        subject: 'Comprueba tu cuenta en APV',
+        text: 'Commprueba tu cuenta en APV',
+        html:`<p> Hola: ${nombre}, comprueba tu cuenta en APV. </p>
+          <p> Tu cuenta ya esta lista, confirmala en el siguiente enlace:
+          <a href="${process.env.FRONTEND_URL}/confirmar/${token}"> Comprobar Cuenta</a> </p>
+
+          <p> Si tu no creaste una cuenta, puedes ignorar este mensaje.</p>
+        `
+      })
+
+      console.log("mensaje enviado:  %s, ", info.messageId);
  };
 
  export default emailRegistro
+
